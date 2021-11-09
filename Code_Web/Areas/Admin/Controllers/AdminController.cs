6e8c8a2b15ca898b019f1ea4f02018c5a36046ea
Tan Dai <daidoan774@gmail.com>
2021-11-09 17:17:50 +0700

    update model

--- Code_Web/Areas/Admin/Controllers/AdminController.cs
+++ Code_Web/Areas/Admin/Controllers/AdminController.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using Code_Web.Models;
 namespace Code_Web.Areas.Admin.Controllers
 {
     public class AdminController : Controller
     {
         // GET: Admin/Admin
+        QLBDSEntities model = new QLBDSEntities();
         public ActionResult Index()
         {
             return View();
